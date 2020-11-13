4025a78f0aea19ab035425cdbc0f57b84a0545b8
JeikuWeiku <jkxdb2002@gmail.com>
2020-11-13 14:57:50 +0000

    Initial commit

---
+++ Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BallPhysics : MonoBehaviour
+{
+    public float thrust = 1.0f; //How far the ball gets yeeted
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //Gets Rigidbody from Unity
+    }
+
+    void FixedUpdate()
+    {
+        //rb.AddForce(transform.forward * thrust);
+    }
+}
